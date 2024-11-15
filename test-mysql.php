8c9c8132aa0cf533cc5fb6a24a328528474943aa
melitoskk <channelvirtooo@gmail.com>
2024-11-15 15:40:03 -0300

    denovo

--- test-mysql.php
+++ test-mysql.php
@@ -6,7 +6,7 @@ error_reporting(E_ALL);
 $host = "localhost";
 $user = "linkit58_admin";
 $password = "^+(<E;Mf%0QFVVT";
-$database = "^+(<E;Mf%0QFVVT";
+$database = "linkit58_main";
 
 // Testar conexão
 $conn = mysqli_connect($host, $user, $password, $database);
@@ -16,3 +16,8 @@ if (!$conn) {
 }
 echo "Conexão bem-sucedida!";
 ?>
+
+$host = "localhost"; // Em HostGator, geralmente é "localhost"
+$user = "linkit58_admin"; // Substitua pelo usuário do banco de dados
+$password = "^+(<E;Mf%0QFVVT"; // Substitua pela senha do banco
+$database = "linkit58_main"; // Substitua pelo nome do banco de dados
