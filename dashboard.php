a83dd4cc5ff023d35e8ab105efb38e5a04719c94
melitoskk <channelvirtooo@gmail.com>
2024-11-17 02:42:17 -0300

    css dashboard

--- dashboard.php
+++ dashboard.php
@@ -15,8 +15,6 @@ function getLojas($conn)
     return $conn->query($sql);
 }
 
-
-// Função para verificar se a loja já existe
 // Função para verificar se a loja já existe
 function verificarLojaExistente($conn, $nome_loja)
 {
@@ -24,32 +22,17 @@ function verificarLojaExistente($conn, $nome_loja)
     return $conn->query($sql)->num_rows > 0;
 }
 
-
-
 // Função para criar loja
 function criarLoja($conn, $nome_loja, $imagem_loja, $link_loja)
 {
-    // Insira a loja com nome, link e imagem
     $sql = "INSERT INTO lojas (nome, link, imagem) VALUES ('$nome_loja', '$link_loja', '$imagem_loja')";
     return $conn->query($sql);
 }
-
-
-
-
 
 // Função para criar categoria
 function criarCategoria($conn, $nome_categoria, $imagem_categoria)
 {
     $sql = "INSERT INTO categorias (nome, imagem) VALUES ('$nome_categoria', '$imagem_categoria')";
-    return $conn->query($sql);
-}
-
-
-// Função para criar episódio
-function criarEpisodio($conn, $categoria_id_episodio, $numero_episodio)
-{
-    $sql = "INSERT INTO episodios (categoria_id, numero_episodio) VALUES ('$categoria_id_episodio', '$numero_episodio')";
     return $conn->query($sql);
 }
 
@@ -61,13 +44,12 @@ function verificarProdutoExistente($conn, $id_produto)
 }
 
 // Função para criar produto
-function criarProduto($conn, $categoria_id_produto, $episodio_id_produto, $nome_produto, $id_produto, $link_produto, $imagem_produto, $loja_produto_id)
+function criarProduto($conn, $categoria_id_produto, $nome_produto, $id_produto, $link_produto, $imagem_produto, $loja_produto_id)
 {
-    $sql = "INSERT INTO produtos (categoria_id, episodio_id, nome_produto, id_produto, link_produto, imagem_produto, loja_produto_id) 
-            VALUES ('$categoria_id_produto', '$episodio_id_produto', '$nome_produto', '$id_produto', '$link_produto', '$imagem_produto', '$loja_produto_id')";
+    $sql = "INSERT INTO produtos (categoria_id, nome_produto, id_produto, link_produto, imagem_produto, loja_produto_id) 
+            VALUES ('$categoria_id_produto', '$nome_produto', '$id_produto', '$link_produto', '$imagem_produto', '$loja_produto_id')";
     return $conn->query($sql);
 }
-
 
 // Conexão com o banco de dados
 $conn = new mysqli($env_ip, "linkit58_admin", "^+(<E;Mf%0QFVVT", "linkit58_main");
@@ -97,10 +79,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
     }
 
-
-
-
-
     // Criar Categoria
     if (isset($_POST['criar_categoria']) && !empty($_POST['nome_categoria']) && !empty($_POST['imagem_categoria'])) {
         $nome_categoria = $_POST['nome_categoria'];
@@ -113,21 +91,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
     }
 
-    // Criar Episódio
-    if (isset($_POST['criar_episodio']) && !empty($_POST['categoria_episodio_id']) && !empty($_POST['numero_episodio'])) {
-        $categoria_id_episodio = $_POST['categoria_episodio_id'];
-        $numero_episodio = $_POST['numero_episodio'];
-        if (criarEpisodio($conn, $categoria_id_episodio, $numero_episodio)) {
-            echo "Episódio criado com sucesso!";
-        } else {
-            echo "Erro ao criar episódio: " . $conn->error;
-        }
-    }
-
     // Criar Produto
-    if (isset($_POST['criar_produto']) && !empty($_POST['categoria_produto_id']) && !empty($_POST['episodio_produto_id']) && !empty($_POST['id_produto']) && !empty($_POST['loja_produto_id'])) {
+    if (isset($_POST['criar_produto']) && !empty($_POST['categoria_produto_id']) && !empty($_POST['id_produto']) && !empty($_POST['loja_produto_id'])) {
         $categoria_id_produto = $_POST['categoria_produto_id'];
-        $episodio_id_produto = $_POST['episodio_produto_id'];
         $nome_produto = $_POST['nome_produto'];
         $id_produto = $_POST['id_produto'];
         $link_produto = $_POST['link_produto'];
@@ -137,7 +103,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if (verificarProdutoExistente($conn, $id_produto)) {
             echo "Produto já existe!";
         } else {
-            if (criarProduto($conn, $categoria_id_produto, $episodio_id_produto, $nome_produto, $id_produto, $link_produto, $imagem_produto, $loja_produto_id)) {
+            if (criarProduto($conn, $categoria_id_produto, $nome_produto, $id_produto, $link_produto, $imagem_produto, $loja_produto_id)) {
                 echo "Produto criado com sucesso!";
             } else {
                 echo "Erro ao criar produto: " . $conn->error;
@@ -159,32 +125,32 @@ $resultCategorias = getCategorias($conn);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gerenciamento de Produtos, Episódios e Categorias</title>
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="dashboard.css">
     <script>
-        // Função para atualizar os episódios com base na categoria selecionada
-        function atualizarEpisodios() {
+        // Função para atualizar as lojas com base na categoria selecionada
+        function atualizarLojas() {
             var categoria_id = document.getElementById("categoria_produto_id").value;
             var xhttp = new XMLHttpRequest();
             xhttp.onreadystatechange = function () {
                 if (this.readyState == 4 && this.status == 200) {
-                    document.getElementById("episodio_produto_id").innerHTML = this.responseText;
+                    document.getElementById("loja_produto_id").innerHTML = this.responseText;
                 }
             };
-            xhttp.open("GET", "buscarEpisodios.php?categoria_id=" + categoria_id, true);
+            xhttp.open("GET", "buscarLojas.php?categoria_id=" + categoria_id, true);
             xhttp.send();
         }
     </script>
 </head>
 
 <body>
-    <h1>Gerenciamento de Produtos, Episódios e Categorias</h1>
 
     <hr>
 
     <!-- Formulário para Criar Loja -->
-    <!-- Formulário para Criar Loja -->
-    <h2>Criar Loja</h2>
+    
     <form method="POST">
+    <h2>Criar Loja</h2>
         <label for="nome_loja">Nome da Loja:</label>
         <input type="text" id="nome_loja" name="nome_loja" required><br><br>
 
@@ -197,12 +163,10 @@ $resultCategorias = getCategorias($conn);
         <button type="submit" name="criar_loja">Criar Loja</button>
     </form>
 
-
-
-
     <!-- Formulário para Criar Categoria -->
-    <h2>Criar Categoria</h2>
+    
     <form method="POST">
+    <h2>Criar Categoria</h2>
         <label for="nome_categoria">Nome da Categoria:</label>
         <input type="text" id="nome_categoria" name="nome_categoria" required><br><br>
 
@@ -212,34 +176,14 @@ $resultCategorias = getCategorias($conn);
         <button type="submit" name="criar_categoria">Criar Categoria</button>
     </form>
 
-
-    <hr>
-
-    <!-- Formulário para Criar Episódio -->
-    <h2>Criar Episódio</h2>
-    <form method="POST">
-        <label for="categoria_episodio_id">Categoria:</label>
-        <select id="categoria_episodio_id" name="categoria_episodio_id" required>
-            <option value="">Selecione uma categoria</option>
-            <?php
-            while ($row = $resultCategorias->fetch_assoc()) {
-                echo "<option value='" . $row['id'] . "'>" . $row['nome'] . "</option>";
-            }
-            ?>
-        </select><br><br>
-
-        <label for="numero_episodio">Número do Episódio:</label>
-        <input type="text" id="numero_episodio" name="numero_episodio" required><br><br>
-        <button type="submit" name="criar_episodio">Criar Episódio</button>
-    </form>
-
     <hr>
 
     <!-- Formulário para Criar Produto -->
-    <h2>Criar Produto</h2>
+    
     <form method="POST">
+    <h2>Criar Produto</h2>
         <label for="categoria_produto_id">Categoria:</label>
-        <select id="categoria_produto_id" name="categoria_produto_id" onchange="atualizarEpisodios()" required>
+        <select id="categoria_produto_id" name="categoria_produto_id" onchange="atualizarLojas()" required>
             <option value="">Selecione uma categoria</option>
             <?php
             // Exibir categorias
@@ -248,11 +192,6 @@ $resultCategorias = getCategorias($conn);
                 echo "<option value='" . $row['id'] . "'>" . $row['nome'] . "</option>";
             }
             ?>
-        </select><br><br>
-
-        <label for="episodio_produto_id">Episódio:</label>
-        <select id="episodio_produto_id" name="episodio_produto_id" required>
-            <option value="">Selecione um episódio</option>
         </select><br><br>
 
         <label for="nome_produto">Nome do Produto:</label>
@@ -286,8 +225,3 @@ $resultCategorias = getCategorias($conn);
 </body>
 
 </html>
-
-<?php
-// Fechar a conexão ao final
-$conn->close();
-?>
